d5dd25d93eebc9df7b45ca1560d9b80806cc40f2
Lee JaeHyun <myleje@gmail.com>
2025-06-10 17:08:11 +0900

    .

--- Assets/02_Scripts/Player/Player.cs
+++ Assets/02_Scripts/Player/Player.cs
@@ -16,15 +16,37 @@ public class Player : MonoBehaviour
         }
     }
 
+    private Collider[] buffer = new Collider[50];
+    
     private Enemy GetClosestEnemy()
     {
         // GC
-        Collider[] enemies = Physics.OverlapSphere(transform.position, 10.0f, 1 << 8);
-
+        //Collider[] enemies = Physics.OverlapSphere(transform.position, 10.0f, 1 << 8);
         // GC
-        var enemiesList = enemies.OrderBy(enemy => Vector3.Distance(transform.position, enemy.transform.position))
-            .ToList();
+        //var enemiesList = enemies.OrderBy(enemy => Vector3.Distance(transform.position, enemy.transform.position)).ToList();
+        //return (enemiesList.Count > 0) ? enemiesList[0].GetComponent<Enemy>() : null;
+
+        int count = Physics.OverlapSphereNonAlloc(transform.position, 10.0f, buffer, 1 << 8);
+
+        if (count == 0) return null;
+
+        // 가장 가까이 있는 적 찾기위한 변수
+        float minDistance = float.MaxValue;
+        Enemy closestEnemy = null;
         
-        return (enemiesList.Count > 0) ? enemiesList[0].GetComponent<Enemy>() : null;
+        for (int i = 0; i < count; i++)
+        {
+            if (buffer[i] == null) break;
+            
+            float distance = Vector3.Distance(transform.position, buffer[i].transform.position);
+            if (distance < minDistance)
+            {
+                closestEnemy = buffer[i].GetComponent<Enemy>();
+                minDistance = distance;
+            }
+        }
+
+        return closestEnemy;
+
     }
 }
